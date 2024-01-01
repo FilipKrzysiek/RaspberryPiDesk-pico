4c3bdd6b7b415f2aba33f9b711db1ef594e43c19
FilipCysEk <krzysiek.swal@gmail.com>
2024-01-01 12:02:21 +0100

    Add predefined text declarations template

---
+++ src/modes/deskDeck/declarations.h
@@ -0,0 +1,13 @@
+//
+// Created by Filip on 01.01.2024.
+//
+
+#ifndef DECLARATIONS_H
+#define DECLARATIONS_H
+#include <string>
+
+const std::string predefiniedText[] = {"", "", "", "", "", "", "", "", ""};
+const std::string predefiniedTextDsc[] = {"-", "-", "-", "-", "-",
+                                          "-", "-", "-", "-"};
+
+#endif // DECLARATIONS_H
